ff8d926e7d5bae059101be90d91628199fbfa512
Filipe Dervelan <filipeedervelan@gmail.com>
2024-04-18 23:12:38 -0300

    feat: first commit

---
+++ app/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+
+
+class Function(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "function"
+        verbose_name_plural = "Functions"
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=40)
+    date_of_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField()
+    function = models.ForeignKey(Function, on_delete=models.PROTECT, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "user"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.name
+
+class Singer(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "singer"
+        verbose_name_plural = "Singers"
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    link = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    singer = models.ForeignKey(Singer, on_delete=models.PROTECT, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "song"
+        verbose_name_plural = "Songs"
+
+    def __str__(self):
+        return f"{self.name} ' - ' {self.description}"
+
+class Ministry(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    participant = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "ministry"
+        verbose_name_plural = "Ministries"
+
+    def __str__(self):
+        return self.name
+
+class Scale(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    song = models.ForeignKey(Song, on_delete=models.PROTECT, null=True, blank=True)
+    participant = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
+    function = models.ForeignKey(Function, on_delete=models.PROTECT, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "scale"
+        verbose_name_plural = "Scales"
+
+    def __str__(self):
+        return self.name
+
+class Unavailability(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "unavailability"
+        verbose_name_plural = "Unavailabilities"
+
+    def __str__(self) -> str:
+        return f"{self.user.name} ' - ' {self.start_date} ' - ' {self.end_date}"
