531f89505b0591c922fff5e10f0890449ead8df1
Filipe Dervelan <filipeedervelan@gmail.com>
2024-04-30 20:46:00 -0300

    feat: models registered in the administrative area

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from app.models import Function, Ministry, Scale, User
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["name",
+                    "email",
+                    "function",
+                    "ministry",
+                    "created_at",
+                    "updated_at"
+    ]
+
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Ministry)
+admin.site.register(Function)
+admin.site.register(Scale)
