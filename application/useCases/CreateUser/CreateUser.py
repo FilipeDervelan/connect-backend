a28199b3ea93219510e16e44089d0e64f469901f
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 17:04:38 -0300

    feat: added treatment by birth_day

--- application/useCases/CreateUser/CreateUser.py
+++ application/useCases/CreateUser/CreateUser.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app.models import User
 from application.useCases.CreateUser.protocols.CreateUserRequest import CreateUserRequest
 from application.useCases.CreateUser.protocols.CreateUserResonse import CreateUserResponse
@@ -31,6 +32,10 @@ class CreateUser:
         email = inbound.email
         password = inbound.password
         birth_day = inbound.birth_day if inbound.birth_day else None
+
+        if birth_day:
+            birth_day = datetime.strptime(birth_day, '%d/%m/%Y')
+            birth_day = birth_day.strftime('%Y-%m-%d')
 
         # Creating user
         newUser = User(name=name, email=email, password=password, birth_day=birth_day)
