3a0d25fb26ffde5a3fe9517a282f2b015f6ff994
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 12:51:56 -0300

    feat: added create function view

---
+++ presentation/controllers/Function/CreateFunctionView.py
@@ -0,0 +1,29 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from application.useCases.CreateFunction.CreateFunction import CreateFunction
+from application.useCases.CreateFunction.protocols.CreateFunctionRequest import CreateFunctionRequest
+
+class CreateFunctionView(APIView):
+    def post(self, request):
+        try:
+            # Building inbound
+            inbound = CreateFunctionRequest()
+            inbound.name = request.data.get("name")
+            inbound.description = request.data.get("description")
+            inbound.ministry_id = request.data.get("ministry_id")
+
+            # Calling use case
+            useCase = CreateFunction()
+            result = useCase.execute(inbound)
+
+            # Serializing
+            outbound = result.__dict__
+
+            return Response({"data": outbound}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
