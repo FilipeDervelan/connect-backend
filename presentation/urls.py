f194013ef863702cd2d43bbc4ba6fd81123106fb
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 12:53:09 -0300

    feat: added new endpoints

--- presentation/urls.py
+++ presentation/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from presentation.controllers.Function.FunctionView import FunctionView
+from presentation.controllers.Function.AssignFunctionView import AssignFunctionView
+from presentation.controllers.Function.CreateFunctionView import CreateFunctionView
+from presentation.controllers.Function.UpdateFunctionView import UpdateFunctionView
+from presentation.controllers.Ministry.AssignMinistryView import AssignMinistryView
 from presentation.controllers.Ministry.CreateMinistryView import CreateMinistryView
 from presentation.controllers.Ministry.DeleteMinistryView import DeleteMinistryView
 from presentation.controllers.Ministry.UpdateMinistryView import UpdateMinistryView
@@ -12,9 +15,13 @@ urlpatterns = [
     path("create-user", CreateUserView.as_view()),
     path("edit-user/<int:id>", EditUserView.as_view()),
 
-    path("create-function", FunctionView.as_view()),
+    path("create-function", CreateFunctionView.as_view()),
+    path("update-function", UpdateFunctionView.as_view()),
 
     path("create-ministry", CreateMinistryView.as_view()),
     path("update-ministry/<int:id>", UpdateMinistryView.as_view()),
     path("delete-ministry/<int:id>", DeleteMinistryView.as_view()),
+
+    path("assign-ministry", AssignMinistryView.as_view()),
+    path("assign-function", AssignFunctionView.as_view()),
 ]
