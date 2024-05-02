f9370566ed652193371960b85c7d6be4bdc24455
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 19:37:52 -0300

    feat: added a check to check if participants are available to be cast

--- application/useCases/CreateScale/CreateScale.py
+++ application/useCases/CreateScale/CreateScale.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from app.models import Ministry, Scale, User
+from django.db.models import Q
+from app.models import Ministry, Scale, Unavailability, User
 from application.useCases.CreateScale.protocols.CreateScaleRequest import CreateScaleRequest
 from application.useCases.CreateScale.protocols.CreateScaleResponse import CreateScaleResponse
 
@@ -32,23 +33,47 @@ class CreateScale:
 
         dateFormatted = datetime.strptime(date, '%d/%m/%Y')
         dateFormatted = dateFormatted.strftime('%Y-%m-%d')
-
-        userList = []
-
-            
+        
         ministry = Ministry.objects.get(id=ministryId)
+
+        exists = Scale.objects.filter(date=dateFormatted, ministry=ministry.id).first()
+
+        if exists:
+            result.response = "A scale alreay exists for this day and this ministry"
+            result.status =  400
+
+            return result
 
         newScale = Scale(name=name, description=description, date=dateFormatted, ministry=ministry)
 
         newScale.save()
 
-        scale = Scale.objects.get(date=dateFormatted)
+        scale = Scale.objects.get(date=dateFormatted, name=name)
+
+        participantsWithUnavailabilities = []
 
         for id in participants:
             userRecovered = User.objects.get(id=id)
+
+            unavailability = Unavailability.objects.filter(
+                Q(user=userRecovered.id) &
+                Q(start_date__lte=dateFormatted) & Q(end_date__gte=dateFormatted) |
+                Q(start_date=dateFormatted) |
+                Q(end_date=dateFormatted)
+            ).first()
+
+            if unavailability:
+                message = f"{userRecovered.name} ({unavailability.start_date} - {unavailability.end_date})"
+
+                participantsWithUnavailabilities.append(message)
+
             scale.participant.add(userRecovered)
+        
+        if len(participantsWithUnavailabilities) > 0:
+            result.response = f"Some participants are not available for the scale: {participantsWithUnavailabilities}"
+        else:
+            result.response = "Scale successfully created"
             
-        result.response = "Scale successfully created"
         result.status = 201
 
         return result
