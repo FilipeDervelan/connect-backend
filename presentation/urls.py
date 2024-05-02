5664fa65a03adc6c6ff9d811c9c24c7564a92db6
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 07:45:30 -0300

    feat: new routes

--- presentation/urls.py
+++ presentation/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from presentation.controllers.Function.FunctionView import FunctionView
 from presentation.controllers.Ministry.CreateMinistryView import CreateMinistryView
+from presentation.controllers.Ministry.DeleteMinistryView import DeleteMinistryView
+from presentation.controllers.Ministry.UpdateMinistryView import UpdateMinistryView
 from presentation.controllers.User.CreateUserView import CreateUserView
 from presentation.controllers.User.EditUserView import EditUserView
 
@@ -13,4 +15,6 @@ urlpatterns = [
     path("create-function", FunctionView.as_view()),
 
     path("create-ministry", CreateMinistryView.as_view()),
+    path("update-ministry/<int:id>", UpdateMinistryView.as_view()),
+    path("delete-ministry/<int:id>", DeleteMinistryView.as_view()),
 ]
