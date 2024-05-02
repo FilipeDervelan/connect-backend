71f0a967050a78e683b713704babe96984b7d744
Filipe Dervelan <filipeedervelan@gmail.com>
2024-05-02 17:04:54 -0300

    feat: added new endpoints

--- presentation/urls.py
+++ presentation/urls.py
@@ -6,14 +6,20 @@ from presentation.controllers.Function.UpdateFunctionView import UpdateFunctionV
 from presentation.controllers.Ministry.AssignMinistryView import AssignMinistryView
 from presentation.controllers.Ministry.CreateMinistryView import CreateMinistryView
 from presentation.controllers.Ministry.DeleteMinistryView import DeleteMinistryView
+from presentation.controllers.Ministry.ListMinistriesView import ListMinistriesView
 from presentation.controllers.Ministry.UpdateMinistryView import UpdateMinistryView
+from presentation.controllers.Scale.CreateScaleView import CreateScaleView
+from presentation.controllers.Scale.ListScalesView import ListScalesView
+from presentation.controllers.Unavailability.CreateUnavailabilityView import CreateUnavailabilityView
 from presentation.controllers.User.CreateUserView import CreateUserView
+from presentation.controllers.User.DeleteUserView import DeleteUserView
 from presentation.controllers.User.EditUserView import EditUserView
 
 
 urlpatterns = [
     path("create-user", CreateUserView.as_view()),
     path("edit-user/<int:id>", EditUserView.as_view()),
+    path("delete-user/<int:id>", DeleteUserView.as_view()),
 
     path("create-function", CreateFunctionView.as_view()),
     path("update-function", UpdateFunctionView.as_view()),
@@ -21,6 +27,12 @@ urlpatterns = [
     path("create-ministry", CreateMinistryView.as_view()),
     path("update-ministry/<int:id>", UpdateMinistryView.as_view()),
     path("delete-ministry/<int:id>", DeleteMinistryView.as_view()),
+    path("list-ministries", ListMinistriesView.as_view()),
+
+    path("create-scale", CreateScaleView.as_view()),
+    path("list-scales", ListScalesView.as_view()),
+
+    path("create-unavailability", CreateUnavailabilityView.as_view()),
 
     path("assign-ministry", AssignMinistryView.as_view()),
     path("assign-function", AssignFunctionView.as_view()),
