9c01f46ab927b1b462d8d60fef87f9194585e9c8
Filipe Dervelan <filipeedervelan@gmail.com>
2024-11-23 12:11:34 -0300

    feat: added register user view

---
+++ presentation/controllers/register_user/register_user_view.py
@@ -0,0 +1,25 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+
+from application.useCases.register_user.register_user import RegisterUser
+
+
+class RegisterUserView(APIView):
+    def post(self, request: Request) -> Response:
+        try:
+            use_case = RegisterUser()
+            outbound = use_case.execute(request.data)
+
+            return Response(
+                outbound.message,
+                status=outbound.status,
+            )
+
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
