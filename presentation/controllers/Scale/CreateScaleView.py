1b3563cf0cfcd2235dca380a57ea15abcfef94e4
Filipe Dervelan <filipeedervelan@gmail.com>
2024-11-24 14:08:14 -0300

    feat: added try exception

--- presentation/controllers/Scale/CreateScaleView.py
+++ presentation/controllers/Scale/CreateScaleView.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,19 +13,25 @@ from application.useCases.CreateScale.protocols.CreateScaleRequest import (
 class CreateScaleView(APIView):
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        inbound = CreateScaleRequest()
-        inbound.name = request.data.get("name")
-        inbound.description = request.data.get("description")
-        inbound.date = request.data.get("date")
-        inbound.participants = request.data.get("participants")
-        inbound.function = request.data.get("function")
-        inbound.song = request.data.get("songs")
-        inbound.ministry_id = request.data.get("ministry_id")
+    def post(self, request: Request) -> Response:
+        try:
+            inbound = CreateScaleRequest()
+            inbound.name = request.data.get("name")
+            inbound.description = request.data.get("description")
+            inbound.date = request.data.get("date")
+            inbound.participants = request.data.get("participants")
+            inbound.song = request.data.get("songs")
+            inbound.ministry_id = request.data.get("ministry_id")
 
-        useCase = CreateScale()
-        result = useCase.execute(inbound)
+            useCase = CreateScale()
+            result = useCase.execute(inbound)
 
-        outbound = result.__dict__
+            outbound = result.__dict__
 
-        return Response(outbound, status=status.HTTP_201_CREATED)
+            return Response(outbound, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response(
+                str(e),
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
